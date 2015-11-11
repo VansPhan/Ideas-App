4b2dead95b3e0d4fe16ce0f3e82332f389d2f99b
Van Phan <vanphan514@gmail.com>
2015-11-11 11:04:44 -0500

    Added comments to login-controller

--- javascript/controllers/login-controller.js
+++ javascript/controllers/login-controller.js
@@ -1,6 +1,8 @@
 angular.module('ideas')
 .controller('LoginController', function($http) {
 	var controller = this;
+	//TODO: Create method for when login button is clicked
+	//TODO: Create method for when user registers
 	$http({ method: 'GET', url: 'http://52.25.89.80/ideas-webservices/api/users' }).success(function(data) {
 		console.log(data);
 		console.log(controller);
