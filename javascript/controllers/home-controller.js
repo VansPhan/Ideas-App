706ce86ecab197d2ff9ea3aa0e112084533676e4
Van Phan <vanphan514@gmail.com>
2015-11-11 15:10:58 -0500

    Added tabs in announcements

--- javascript/controllers/home-controller.js
+++ javascript/controllers/home-controller.js
@@ -1,15 +1,24 @@
 angular.module('ideas')
 .controller('HomeController', ['$scope', function($scope) {
-	$scope.announcement = {
-		"user" : "van",
-		"message" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-	};
-	$scope.posts = [
+	$scope.announcements = messages;
+	$scope.posts = posts;
+
+}]);
+
+var messages = [
+	{
+		"user" : "Van",
+		"message" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." 
+	},
+	{
+		"user" : "Clifford",
+		"message" : "I am Clifford"
+	}];
+
+var posts = [
 		{ "message" : "post1" },
 		{ "message" : "post2" },
 		{ "message" : "post3" },
 		{ "message" : "post4" },
 		{ "message" : "post5" }
-	]
-
-}]);
+	];
