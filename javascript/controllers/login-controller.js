f21118cf6c01e6bfcbea88c2beffc6496746a422
Van Phan <vanphan514@gmail.com>
2015-11-16 23:06:01 -0500

    Made if statement to switch cases in login controller

--- javascript/controllers/login-controller.js
+++ javascript/controllers/login-controller.js
@@ -6,15 +6,11 @@ angular.module('ideas')
 		for (var i = 0; i < $scope.users.length; i++) {
 			console.log($scope.users[i]);
 		};
-		// 	switch(email) {
-		// 		case 
-		// 	}
-		// 	if (val['email'] == email) {
-		// 		$location.path('/home');
-		// 	} else {
-		// 		$location.path('/fail-login');
-		// 	};
-		});
+		// if (val['email'] == email) {
+		// 	$location.path('/home');
+		// } else {
+		// 	$location.path('/fail-login');
+		// };
 	};
 	$http({ method: 'GET', url: 'http://localhost/ideas-webservices/api/users' }).success(function(data) {
 		$scope.users = data;
