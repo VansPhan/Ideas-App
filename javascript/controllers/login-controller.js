c5f80ccc4fadf98926965f467befae5e30fef224
Van Phan <VanPhan514@gmail.com>
2015-11-17 22:29:38 -0500

    Removed .ttf file and added dummy data to test login controller

--- javascript/controllers/login-controller.js
+++ javascript/controllers/login-controller.js
@@ -1,7 +1,7 @@
 angular.module('ideas')
 .controller('LoginController', function($http, $scope, $location) {
 	$scope.mainUser;
-	$scope.users;
+	$scope.users = dummy_users;
 	$scope.login = function(email) {
 		for (var i = 0; i < $scope.users.length; i++) {
 			console.log($scope.users[i]);
@@ -12,8 +12,30 @@ angular.module('ideas')
 		// 	$location.path('/fail-login');
 		// };
 	};
-	$http({ method: 'GET', url: 'http://localhost/ideas-webservices/api/users' }).success(function(data) {
-		$scope.users = data;
-		console.log($scope.users);
-	});
+
+	// $http({ method: 'GET', url: 'http://localhost:8080/ideas-webservices/api/users' }).success(function(data) {
+	// 	$scope.users = data;
+	// 	console.log($scope.users);
+	// });
 });
+
+
+var dummy_users = 
+	[{
+		"dateCreated":"2015-10-31T00:00:00",
+		"email":"test@wwindtech.com",
+		"firstName":"testFirstName",
+		"lastName":"testLastName",
+		"status":"Active",
+		"userId":1,
+		"userType":"user"
+	},
+	{
+		"dateCreated":"2015-10-31T00:00:00",
+		"email":"test1@wwindtech.com",
+		"firstName":"test",
+		"lastName":"test",
+		"status":"Active",
+		"userId":2,
+		"userType":"admin"
+	}];
