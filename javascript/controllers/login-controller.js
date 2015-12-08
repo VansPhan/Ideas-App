a07a3d05343c8307777f732a2189c5824c10004d
Van Phan <VanPhan514@gmail.com>
2015-12-07 19:09:02 -0500

    Removed login.js

--- javascript/controllers/login-controller.js
+++ javascript/controllers/login-controller.js
@@ -2,9 +2,23 @@ angular.module('ideas')
 .controller('LoginController', function($scope, $location, account) {
 	$scope.users;
 	$scope.log = function(email) {
-		console.log(email);
+		if ($scope.users.length == undefined) {
+			if ($scope.users['email'] == email) {
+					$location.path('/home');
+				} else {
+					$location.path('/fail-login');
+				}
+		} else {
+			for (i = 0; i < $scope.users.length; i++) {
+				if ($scope.users[i]['email'] == email) {
+						$location.path('/home');
+						break;
+				} else {
+					$location.path('/fail-login');
+				};
+			};
+		};
 	};
-	console.log('hello');
 	account.all()
 	.success(function(data) {
 		$scope.users = data;
